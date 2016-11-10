49c035ed283a6e514c856bd31e5c96cebc685868
Pawel Grad <pawelgradd@gmail.com>
2016-11-10 15:03:06 +0100

    Clean

--- Src/tim.c
+++ Src/tim.c
@@ -57,9 +57,9 @@ void MX_TIM7_Init(void)
   TIM_MasterConfigTypeDef sMasterConfig;
 
   htim7.Instance = TIM7;
-  htim7.Init.Prescaler = 9999;
+  htim7.Init.Prescaler = 167;
   htim7.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim7.Init.Period = 4999;
+  htim7.Init.Period = 10000;
   if (HAL_TIM_Base_Init(&htim7) != HAL_OK)
   {
     Error_Handler();
